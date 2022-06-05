338820a7f83593b207797e2c61fa9b0a6f5709a8
jayers92 <99706818+jayers92@users.noreply.github.com>
2022-06-05 14:16:01 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,19 +22,45 @@
  * Define Global Variables
  * 
 */
-const sectionElem = document.querySelectorAll('section');
+const sectionElem = document.querySelectorAll("section");
 
 for (let i = 0; i < sectionElem.length; i = i + 1) {
-    const idElem = document.getElementById('navbar__list');
+  const idElem = document.getElementById("navbar__list");
 
-    const sectionValue = sectionElem[i].id;
-    
-    const sElem = document.createElement('li');
-    sElem.innerText = sectionValue;
-    navbar__list.appendChild(sElem);
-  }
+  const sectionValue = sectionElem[i].id;
+
+  const sElem = document.createElement("li");
+  // create the anchor tag
+  const aElem = document.createElement("a");
+  aElem.classList.add("menu__link");
+  aElem.innerText = sectionValue;
+  sElem.appendChild(aElem);
+
+  aElem.addEventListener("click", () => {
+    sectionElem[i].scrollIntoView({ behavior: "smooth" });
+  });
+
+  //const navBarMenu = document.getElementById('aElem');
+  // create a click event listener on the anchor tag
+  // on click, scroll sectionElem[i] into view
+
+  idElem.appendChild(sElem);
+}
 
 // Add class 'active' to section when near top of viewport
+
+// Add class 'active' to section when near top of viewport
+const section = document.querySelectorAll("section");
+window.addEventListener("scroll", () => {
+  for (let i = 0; i < section.length; i = i + 1) {
+    const areaBounds = section[i].getBoundingClientRect();
+    if (areaBounds.top > 0 && areaBounds.bottom < window.innerHeight) {
+      section[i].classList.add("your-active-class");
+    } else {
+      section[i].classList.remove("your-active-class");
+    }
+  }
+});
 
 
 // Scroll to anchor ID using scrollTO event
