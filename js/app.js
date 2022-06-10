1ac78995374bc267c919a4df4a626b03e9a1efb8
jayers92 <99706818+jayers92@users.noreply.github.com>
2022-06-10 09:33:22 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,11 +50,39 @@ const sectionElem = document.querySelectorAll("section");
  // Add class 'active' to section when near top of viewport
  
  // Add class 'active' to section when near top of viewport
+ const sectionElem = document.querySelectorAll("section");
+
+ for (let i = 0; i < sectionElem.length; i = i + 1) {
+   const idElem = document.getElementById("navbar__list");
+ 
+   const sectionValue = sectionElem[i].id;
+ 
+   const sElem = document.createElement("li");
+   // create the anchor tag
+   const aElem = document.createElement("a");
+   aElem.classList.add("menu__link");
+   aElem.innerText = sectionValue;
+   sElem.appendChild(aElem);
+ 
+   aElem.addEventListener("click", () => {
+     sectionElem[i].scrollIntoView({ behavior: "smooth" });
+   });
+ 
+   //const navBarMenu = document.getElementById('aElem');
+   // create a click event listener on the anchor tag
+   // on click, scroll sectionElem[i] into view
+ 
+   idElem.appendChild(sElem);
+ }
+ 
+ // Add class 'active' to section when near top of viewport
+ 
+ // Add class 'active' to section when near top of viewport
  const sections = document.querySelectorAll("section");
  window.addEventListener("scroll", () => {
    for (let i = 0; i < sections.length; i = i + 1) {
      const areaBounds = sections[i].getBoundingClientRect();
-     if (areaBounds.top > 0 && areaBounds.bottom < window.innerHeight) {
+     if (areaBounds.top < window.innerHeight) {
        sections[i].classList.add("your-active-class");
        const aTag = document.querySelectorAll("a");
        for (let j = 0; j < aTag.length; j = j + 1) {
